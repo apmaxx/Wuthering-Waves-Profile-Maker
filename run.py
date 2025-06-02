a2b23192544ffcb57c9e79d93ddb9692a705a03f
Amir Pettigew <amirpacpett@gmail.com>
2025-06-02 00:52:45 -0400

    modified:   run.py 	modified:   static/indexstyles.css 	new file:   templates/aalto.html 	new file:   templates/calcharo.html 	modified:   templates/characters.html

--- run.py
+++ run.py
@@ -3,10 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/characters')
-def wuwa_login():
-
-
+def wumir_characters():
     return  render_template ("characters.html")
+    
+
+@app.route('/characters/aalto')
+
+def aalto_build():
+    return render_template("aalto.html")
+
+@app.route('/characters/calcharo')
+
+def calcharo_build():
+    return render_template("calcharo.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
