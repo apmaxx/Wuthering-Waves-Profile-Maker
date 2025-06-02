b754d9e5affa5824e2e6fa2bc18754c65bf20087
Amir Pettigew <amirpacpett@gmail.com>
2025-06-01 22:10:42 -0400

    modified:   run.py 	modified:   static/indexstyles.css 	modified:   templates/characters.html

--- run.py
+++ run.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/characters')
 def wuwa_login():
 
 
-    return  render_template ("index.html")
+    return  render_template ("characters.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
